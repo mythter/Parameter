e8a78d3f4ea2f1c06ac98bb2b9ee4d44a1dedc9e
Anton Polozhyi <apoloz@softserveinc.com>
2026-01-20 20:14:50 +0200

    added EnterFocusBehavior

---
+++ Parameter/AttachedProperties/EnterFocusBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace Parameter.AttachedProperties
+{
+	public static class EnterFocusBehavior
+	{
+		public static readonly AttachedProperty<IInputElement?> NextFocusProperty =
+			AvaloniaProperty.RegisterAttached<Control, Control, IInputElement?>("NextFocus");
+
+		static EnterFocusBehavior()
+		{
+			InputElement.KeyDownEvent.AddClassHandler<Control>(OnKeyDown);
+		}
+
+		private static void OnKeyDown(Control control, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter &&
+				GetNextFocus(control) is IInputElement next)
+			{
+				next.Focus();
+				e.Handled = true;
+			}
+		}
+
+		public static void SetNextFocus(Control control, IInputElement value) =>
+			control.SetValue(NextFocusProperty, value);
+
+		public static IInputElement? GetNextFocus(Control control) =>
+			control.GetValue(NextFocusProperty);
+	}
+}
