56b50c8a2b901ef70a291f8976aa90f3f7310cbb
Anton Polozhyi <apoloz@softserveinc.com>
2026-01-22 11:16:16 +0200

    added AutoCompleteEnterBehavior

---
+++ Parameter/Behaviors/AutoCompleteEnterBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+
+namespace Parameter.Behaviors
+{
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S1121:Assignments should not be made from within sub-expressions", Justification = "It's a new syntax")]
+	public class AutoCompleteEnterBehavior : Behavior<AutoCompleteBox>
+	{
+		public static readonly StyledProperty<ICommand?> CommandProperty =
+			AvaloniaProperty.Register<AutoCompleteEnterBehavior, ICommand?>(nameof(Command));
+
+		public ICommand? Command
+		{
+			get => GetValue(CommandProperty);
+			set => SetValue(CommandProperty, value);
+		}
+		protected override void OnAttached()
+		{
+			AssociatedObject?.KeyDown += OnKeyDown;
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject?.KeyDown -= OnKeyDown;
+		}
+
+		private void OnKeyDown(object? sender, KeyEventArgs e)
+		{
+			if (e.Key != Key.Enter)
+				return;
+
+			if (Command?.CanExecute(null) == true)
+				Command.Execute(null);
+		}
+	}
+}
