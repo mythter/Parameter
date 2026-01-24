4e15149a2c5fd777474d3aa29cc75afa6b236b23
Anton Polozhyi <apoloz@softserveinc.com>
2026-01-24 17:28:28 +0200

    added AutoCompleteKeyDownBehavior and AutoCompleteLostFocusBehavior

---
+++ Parameter/Behaviors/AutoCompleteKeyDownBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+
+namespace Parameter.Behaviors
+{
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S1121:Assignments should not be made from within sub-expressions", Justification = "<Pending>")]
+	public class AutoCompleteKeyDownBehavior : Behavior<AutoCompleteBox>
+	{
+		public static readonly StyledProperty<ICommand?> CommandProperty =
+			AvaloniaProperty.Register<AutoCompleteKeyDownBehavior, ICommand?>(nameof(Command));
+
+		public ICommand? Command
+		{
+			get => GetValue(CommandProperty);
+			set => SetValue(CommandProperty, value);
+		}
+
+		protected override void OnAttached()
+		{
+			AssociatedObject?.KeyDown += OnKeyDown;
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject?.KeyDown -= OnKeyDown;
+		}
+
+		private void OnKeyDown(object? sender, KeyEventArgs e)
+		{
+			if (Command?.CanExecute(e) == true)
+				Command.Execute(e);
+		}
+	}
+}
---
+++ Parameter/Behaviors/AutoCompleteLostFocusBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Input;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Parameter.Behaviors
+{
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S1121:Assignments should not be made from within sub-expressions", Justification = "<Pending>")]
+	public sealed class AutoCompleteLostFocusBehavior : Behavior<AutoCompleteBox>
+	{
+		public static readonly StyledProperty<ICommand?> CommandProperty =
+			AvaloniaProperty.Register<AutoCompleteLostFocusBehavior, ICommand?>(nameof(Command));
+
+		public ICommand? Command
+		{
+			get => GetValue(CommandProperty);
+			set => SetValue(CommandProperty, value);
+		}
+
+		public static readonly StyledProperty<object?> CommandParameterProperty =
+			AvaloniaProperty.Register<AutoCompleteLostFocusBehavior, object?>(
+				nameof(CommandParameter));
+
+		public object? CommandParameter
+		{
+			get => GetValue(CommandParameterProperty);
+			set => SetValue(CommandParameterProperty, value);
+		}
+
+		protected override void OnAttached()
+		{
+			AssociatedObject?.LostFocus += OnLostFocus;
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject?.LostFocus -= OnLostFocus;
+		}
+
+		private void OnLostFocus(object? sender, RoutedEventArgs e)
+		{
+			var parameter = CommandParameter ?? AssociatedObject?.Text;
+
+			if (Command?.CanExecute(parameter) == true)
+			{
+				Command.Execute(parameter);
+			}
+		}
+	}
+}
